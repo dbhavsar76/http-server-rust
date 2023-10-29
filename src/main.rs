cab5d1a9b8ac89e889c228c6d2735f91bc03698f
Dhruv Bhavsar <dhruv.bhavsar@optimoz.in>
2023-10-29 16:05:39 +0530

    return 404 or 200 based on path

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io::Write;
-use std::net::{Shutdown, TcpListener};
+use std::io::{Read, Write};
+use std::net::{Shutdown, TcpListener, TcpStream};
+use itertools::Itertools;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,14 +9,41 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for _stream in listener.incoming() {
         match _stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes());
-                stream.shutdown(Shutdown::Both);
+                handle_request(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_request(mut stream: TcpStream) {
+    let mut request = String::new();
+    stream.read_to_string(&mut request);
+    stream.shutdown(Shutdown::Read);
+
+    let path = get_request_path(request);
+    let response = match path.as_str() {
+        "/" => "HTTP/1.1 200 OK\r\n\r\n",
+        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+    };
+
+    stream.write_all(response.as_bytes());
+    stream.flush();
+    stream.shutdown(Shutdown::Write);
+}
+
+fn get_request_path(request: String) -> String {
+    let sections = request.split("\r\n\r\n").collect_vec();
+
+    let head = sections[0];
+    let parts = head.split("\r\n").collect_vec();
+
+    let start_line = parts[0];
+    let path = start_line.split_whitespace().nth(1).unwrap();
+
+    path.to_string()
 }
